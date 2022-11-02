67306b6cfd8357ba2e3ecac16e0f2af64e297c10
Lars Bosales <larsjohanbosales@gmail.com>
2022-11-02 15:00:06 +1300

    V1.1.0
    Buttons for main GUI sorted

--- BookingController.java
+++ BookingController.java
@@ -5,10 +5,34 @@
  */
 package Assignment2;
 
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  *
  * @author Lars Bosales, 20112844
  */
-public class BookingController {
+public class BookingController extends JFrame{
+
+    //========= Fields =========
+    //Based on MVC model
+    private Booking model; //M
+    private BookingView view; //V
+   
     
+    //========= Constructor =========
+    public BookingController() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //stop runtime on exit
+        setSize(300, 300); //frame size
+        this.view = new BookingView(this.model); //instantiate the panel
+        add(view); //add to the Frame
+        
+        //========= Action Listeners =========
+    }
+    
+        //========= Main =========
+    public static void main(String[] args) {
+        JFrame myFrame = new BookingController(); //create the frame
+        myFrame.setVisible(true);
+    }
 }
--- BookingView.java
+++ BookingView.java
@@ -6,34 +6,73 @@
 package Assignment2;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author Lars Bosales, 20112844
  */
-public class BookingView{
-    
+public class BookingView extends JPanel {
+
     //========= Fields =========
-    private Booking model; 
-    private JPanel panel;
-    
+    private Booking model;
+
     //Buttons
     private JButton bookingButton;
-    private JButton viewAvailabilityButton;
+    private JButton availabilityButton;
+    private JButton currencyButton;
+    private JButton reviewButton;
+    private JButton contactButton;
+
     //Labels
-    
+    private JLabel welcomeTitle;
     //TextFields
-    
+
     //========= Constructor =========
-    public BookingView(Booking model){ //will initialise the components
+    public BookingView(Booking model) { //will initialise the components
         //initialise
-        panel = new JPanel();
         this.model = model;
-        panel.setLayout(null);
+        setLayout(null);
+
+        //## Create the components and then add to the panel ##
         
-        //== Create the components and then add to the panel ==
         //===== Buttons =====
+        //bookingButton attributes
+        bookingButton = new JButton("Create Booking");
+        bookingButton.setLocation(65, 50);
+        bookingButton.setSize(150, 30);
+        add(bookingButton);
+
+        //availabilityButton attributes
+        availabilityButton = new JButton("View Availability");
+        availabilityButton.setLocation(65, 90);
+        availabilityButton.setSize(150, 30);
+        add(availabilityButton);
+        
+        //currencyButton attributes
+        currencyButton = new JButton("Currency Converter");
+        currencyButton.setLocation(65, 130);
+        currencyButton.setSize(150, 30);
+        add(currencyButton);
+        
+        //reviewButton attributes
+        reviewButton = new JButton("Reviews");
+        reviewButton.setLocation(65, 170);
+        reviewButton.setSize(150, 30);
+        add(reviewButton);
+        
+        //contactButton attributes
+        contactButton = new JButton("Contact Details");
+        contactButton.setLocation(65, 210);
+        contactButton.setSize(150, 30);
+        add(contactButton);
+        
+        //===== Labels =====
+        welcomeTitle = new JLabel("## Welcome to Dreamland Hotels! ##");
+        welcomeTitle.setLocation(35, 15);
+        welcomeTitle.setSize(250, 20);
+        add(welcomeTitle);
     }
-    
+
 }
